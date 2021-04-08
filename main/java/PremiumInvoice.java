77c4409ddd5d50f062ea2975da444536733cf10f
saitarun135 <saitarun135@gm.com>
2021-04-08 10:15:24 +0530

    [sai]Added: premium rides,minimum,monthly fare

--- main/java/PremiumInvoice.java
+++ main/java/PremiumInvoice.java
@@ -1,7 +1,8 @@
-public class InvoiceGenerator {
-    private static final double MINIMUM_COST_PER_KM = 10;
-    private static final double COST_PER_TIME = 1;
-    private static final double MINIMUM_FARE = 5;
+public class PremiumInvoice {
+    private static final double MINIMUM_COST_PER_KM = 15;
+    private static final double COST_PER_TIME = 2;
+    private static final double MINIMUM_FARE = 20;
+
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
